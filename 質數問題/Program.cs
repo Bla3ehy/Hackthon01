5d184af332284fa47186f19f5332cd33f3f4740d
C1ozer <53912176+Bla3ehy@users.noreply.github.com>
2020-03-02 23:52:04 +0800

    Add files via upload

---
+++ 質數問題/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 質數問題
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 101; i <= 200; i++)
+            {
+                bool isFind = false;
+                for (int y = 2; y < i; y++)
+                {
+                    if (i % y == 0)
+                    {
+                        isFind = true;
+                    }
+                }
+                if (!isFind)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+}
+
+
